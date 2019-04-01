0183e2bd3e54ff3e72a74df46e377d2b67a5d577
river-ceanne <snvencer@gmail.com>
2019-04-01 14:08:53 -0700

    scaffolding

---
+++ app.js
@@ -0,0 +1,13 @@
+
+'use strict';
+
+var location1 = {
+  name: '1st and Pike',
+  minCust: 0,
+  maxCust: 0,
+  avgCust: 0.0,
+  randomCust: function(){
+    return Math.floor(Math.random() * Math.floor(max));
+  }
+
+};
